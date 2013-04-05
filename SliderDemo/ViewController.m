9b8ae21f62f45befe51b6b85724900f69ae1c780
DucHa <ducha@DucHas-iMac.local>
2013-04-05 10:26:18 +0700

    abc

--- SliderDemo/ViewController.m
+++ SliderDemo/ViewController.m
@@ -9,7 +9,8 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property(weak,nonatomic) IBOutlet UISlider *sliderB;
+@property(weak,nonatomic) IBOutlet UILabel *labelB;
 @end
 
 @implementation ViewController
@@ -18,12 +19,18 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-}
+    _labelB.textColor = [UIColor blueColor];
+    _labelB.text = [NSString stringWithFormat:@"%.2f",_sliderB.value];
+    }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
+- (IBAction)sliderBChanged:(id)sender {
+    float x = _sliderB.value * 3.02;
+    _labelB.frame = CGRectMake(_sliderB.value, 120, 50, 20);
+    _labelB.text = [NSString stringWithFormat:@"%.2f",_sliderB.value];
+}
 @end
